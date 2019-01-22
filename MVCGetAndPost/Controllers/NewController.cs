cf8e3dd14fff7208e3c0a9e40753fec839669abb
Mustafa Yıldırım <39571054+rootercs@users.noreply.github.com>
2019-01-22 17:32:57 +0300

    Initial commit

---
+++ MVCGetAndPost/Controllers/NewController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCGetAndPost.Controllers
+{
+    public class NewController : Controller
+    {
+        // GET: New
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult login()
+        {
+            //post: son kullanicinin sayfadan dbye ya da bir dökümana veri gondermek ve sayfa uzerine girilen verileri yakalamak amacıyla kullanilir.
+
+            //if (Request.HttpMethod == "POST")
+            //{
+            //    string userName = Request.Form.Get("txtKullaniciAdi");
+            //    string userPassword = Request.Form.Get("txtSifre");
+            //    ViewBag.Password = userPassword;
+            //    ViewBag.userName = userName;
+            //}
+
+            return View();
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult login(FormCollection frm)
+        {
+            string userName = frm.Get("txtKullaniciAdi");
+            string userPassword = frm.Get("txtSifre");
+            ViewBag.Password = userPassword;
+            ViewBag.userName = userName;
+            return View();
+        }
+        [AcceptVerbs(HttpVerbs.Get)]
+        public ActionResult GetMethod(string ID)
+        {
+            ViewBag.GetId = ID;
+            return View();
+        }
+    }
+}
